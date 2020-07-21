80ae8b8434f7aced73d44723cdba2deec987b3d1
Anishwar <50guptaaniswar@gmail.com>
2020-07-21 23:50:08 +0530

    homepage into components

---
+++ src/pages/Homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from '../../components/directory/directory.component'
+const Homepage = (props) =>(
+    <div className="Homepage">
+        <Directory />
+    </div>
+);
+export default Homepage;
