475bbfdc68f9e84745327d6a673615b6010490a7
Anishwar <50guptaaniswar@gmail.com>
2020-07-23 14:17:19 +0530

    Navigation applied

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import {Route, Switch,BrowserRouter} from 'react-router-dom';
 import Homepage from './pages/Homepage/homepage.component';
 import './App.css';
 
+
+const Hatspage = () =>(
+  <div>
+    <h1> Hats Page</h1>
+  </div>
+)
 function App() {
-  return (
-    <div >
-    <Homepage />
-    </div>
+  return ( 
+    <BrowserRouter>
+    <div>
+    <Switch>
+     <Route exact  path='/' component={Homepage}/>
+     <Route  path='/hats' component={Hatspage}/>
+    
+    </Switch></div>
+    </BrowserRouter>
   );
 }
 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import "./menu-item.styles.scss";
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({title,imageUrl,size}) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({title,imageUrl,size,history,linkUrl,match}) => (
+    <div className={`  ${size} menu-item`} onClick={ () =>  history.push(`${match.url}${linkUrl}`)}>
         <div className="background-image" style={{
           backgroundImage: `url(${imageUrl})`
         }}
         />
                 <div className="content">
                     <h1 className='title'>{title.toUpperCase()}</h1>
-                    <span classNam='subtitle'>SHOW NOW</span>
+                    <span className='subtitle'>SHOW NOW</span>
                 </div>
             </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem) ;
